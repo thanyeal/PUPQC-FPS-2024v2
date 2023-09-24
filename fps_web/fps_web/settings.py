5192927ad84e8027396ea30107309476c58c763d
daenyuhl <128719578+daenyuhl@users.noreply.github.com>
2023-09-24 14:44:16 +0800

    adjusted UI in log in, added pic of Obelisk

--- fps_web/fps_web/settings.py
+++ fps_web/fps_web/settings.py
@@ -154,10 +154,13 @@ STATICFILES_DIRS = [BASE_DIR / 'fps_web/static']
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Other essentials
 
 LOGIN_URL = '/login'
 
