70e2cac84362621d14bb83d5b06266d04ac7947e
Daniel Reyes <128719578+daenyuhl@users.noreply.github.com>
2023-10-14 19:02:27 +0800

    asdxcxcqwe

--- fps_web/fps_web/settings.py
+++ fps_web/fps_web/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
